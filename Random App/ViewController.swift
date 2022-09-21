e32b9758bacf4ea9bdef18da54526f0ef4cc3302
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2022-09-21 09:10:25 +0700

    Initial Commit

---
+++ Random App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random App
+//
+//  Created by Wolf on 21/9/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
